8fc60e14850bf953fefc97d0bee4ae272050be43
Chantal Wuerschinger <chantalwue@gmail.com>
2023-02-07 22:44:06 +0100

    Added descriptions, emojis and paragraphs for better UX

--- snippet_converter.py
+++ snippet_converter.py
@@ -1,11 +1,16 @@
 
 
 def get_inputs():
-    title = input('What is the title of your snippet? ')
-    prefix = input('What is the prefix of the snippet? ')
+    '''
+    This method gets and returns the user's input for title, prefix,
+    body and description.
+    '''
+
+    title = input('What is the title of your snippet?\n')
+    prefix = input('What is the prefix of the snippet?\n')
     # body = input('Please enter your code divided by ; after each line: ')
 
-    print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it.")
+    print("Enter/Paste your code block. Ctrl-D or Ctrl-Z (Win) to save it.\n")
     body = []
     while True:
         try:
@@ -13,16 +18,16 @@ def get_inputs():
         except EOFError:
             break
         body.append(line)
-    description = input('Describe your code snippet: ')
+    description = input('\nDescribe your code snippet:\n')
 
-    breakpoint()
     return title, prefix, body, description
 
-def snippet_transformer(block):
-    lines = block.split('# ')[1:]
+def snippet_transformer(body):
+    # lines = body.split('# ')[1:]
     results = []
 
-    for line in lines:
+    for line in body:
+        # line.strip('#')
         leading_space = len(line) - len(line.lstrip())
         space = leading_space * ' '
         results.append(f'"{space}{line.strip()}",')
@@ -32,6 +37,9 @@ def snippet_transformer(block):
 def print_snippet(title, prefix, body, description):
     results = snippet_transformer(body)
 
+    print('\nDone. ✅')
+    print('\nCopy and paste this into your snippet .json file: \n')
+    print('⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️\n')
     print(f'"{title}": {{')
     print(f'  "prefix": "{prefix}",')
     print(f'  "body": [')
@@ -42,6 +50,7 @@ def print_snippet(title, prefix, body, description):
     print(f'  "description": "{description}"')
     print(f'}}')
 
+    print('\n⬆️ ⬆️ ⬆️ ⬆️ ⬆️ ⬆️ ⬆️ ⬆️ ⬆️ ⬆️ ')
     return ""
 
 
